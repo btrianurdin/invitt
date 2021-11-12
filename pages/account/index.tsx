210d59427faad6f2cd002f924b1be7d75059b3bd
B Trianurdin <btstargroup@gmail.com>
2021-11-13 00:24:57 +0700

    fix: add br

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -13,10 +13,11 @@ export default function Account(): JSX.Element {
         <Head>
           <title>{titleName('Home')}</title>
         </Head>
-        <div css={tw`my-5 font-family['Poppins'] grid grid-cols-2 gap-14`}>
+        <div css={tw`my-5 font-family['Poppins'] md:(grid grid-cols-2 gap-14)`}>
           <EditAccount />
-          <div>
+          <div tw="mt-12 md:mt-0">
             <h1 tw="text-xl font-medium pb-1.5 border-b border-pink-400 inline-block">Change Password</h1>
+            <br />
             <Button text="Change Password" color="pink" tw="my-6 px-6 py-2.5 " />
           </div>
         </div>
