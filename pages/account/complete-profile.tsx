87829bb61f3ea13a9c11ac84211ad847ed01cc02
B Trianurdin <btstargroup@gmail.com>
2021-11-12 20:03:16 +0700

    fix: some problem (styles file) and eslint when build

--- pages/account/complete-profile.tsx
+++ pages/account/complete-profile.tsx
@@ -1,13 +1,44 @@
 import Head from 'next/head';
 import { useState } from 'react';
-import tw from 'twin.macro';
+import tw, { css } from 'twin.macro';
 import Button from '../../components/atoms/Button';
 import ChooseTemplate from '../../components/molecules/ChooseTemplate';
 import CompleteProfileForm from '../../components/molecules/CompleteProfileForm';
 import { LayoutAuth } from '../../components/organisms/Layout';
 import RouteRegister from '../../components/organisms/Route/RouteRegister';
-import { titleName } from '../../constants/commons';
-import styles from './styles';
+import { titleName, colorTheme } from '../../constants/commons';
+
+const styles = {
+  tab: tw`md:width[600px] bg-white shadow-lg mx-auto my-12 rounded-sm font-family['Poppins']`,
+  tabMenu: css`
+    ${tw`w-full border-b flex flex-col md:flex-row`};
+    & button{
+      ${tw`p-5 flex-1`}
+      &:hover{
+        ${tw`bg-gray-200`}
+      }
+      &.active{
+        ${tw`transition-colors duration-300`};
+        background-color: ${colorTheme.pink};
+        color: #fff;
+      }
+    }
+  `,
+  tabBody: css`
+    ${tw`p-5`};
+    & > h3{
+      ${tw`text-2xl py-3 text-center`};
+      color: ${colorTheme.pink};
+    }
+  `,
+  tabContent: css`
+    ${tw`hidden py-3`};
+    &.active{
+      ${tw`block`}
+    }
+  `,
+  btnDisabled: tw`cursor-not-allowed`,
+};
 
 export default function CompleteProfile(): JSX.Element {
   const [tabToggle, setTabToggle] = useState(1);
